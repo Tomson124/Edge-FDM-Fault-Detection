521152e67324f7fd6d8ab384aa617853126b4011
oliverbravery <dev@oliverbravery.uk>
2025-04-24 10:19:29 +0100

    Add licensing information to all relevant files, citing the original Prototypical Networks implementation

--- prototypical-network-training/setup.py
+++ prototypical-network-training/setup.py
@@ -1,3 +1,8 @@
+"""
+This code was adapted from the original implementation of Prototypical Networks for Few-Shot Learning.
+Link: https://github.com/jakesnell/prototypical-networks
+"""
+
 from setuptools import setup, find_packages
 
 setup(name='protonets',
